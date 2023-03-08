224054dd728a9e1ebdd501664761d3d93f693786
Syuzanna <nersisyansuzi@gmail.com>
2023-03-08 16:34:32 +0400

    Update Details.js

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -22,7 +22,7 @@ function Details() {
                 {Item.map((elem) => {
                     return (
                         <S.DivItem key={elem.id}>
-                            <S.DivLeft src={`/img/` + elem.src + ".png"}></S.DivLeft>
+                            <S.DivLeft src={`img/` + elem.src + ".png"}></S.DivLeft>
                             <S.DivRight>
                                 {elem.composition}
                                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
